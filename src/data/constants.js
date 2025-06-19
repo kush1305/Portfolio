c2234134f8ca976ba5cbffa971ea183d75aca4fe
Kush Kumar <kush6279@gmail.com>
2025-06-19 20:41:09 +0530

    resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/kush1305",
     resume:
-      "https://drive.google.com/file/d/1RwZNAf7biW5619tVKgbtpcaAIGQmkdDd/view?usp=drive_link",
+      "https://drive.google.com/drive/folders/1qsdcdKsWsZN1vJnjO04Q1I1hlk-kv4ix",
     linkedin: "",
     twitter: "",
     insta: "",
